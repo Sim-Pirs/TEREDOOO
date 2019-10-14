8f4e11c8922fdcf47dda30b8cf73d54cb32fc4d4
Sim-Pirs <43365681+Sim-Pirs@users.noreply.github.com>
2019-10-14 17:31:34 +0200

    Add files via upload

---
+++ TUN/tunalloc.h
@@ -0,0 +1,18 @@
+#ifndef TUNALLOC_H
+#define TUNALLOC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h> 
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+int tun_alloc(char *dev);
+
+#endif
